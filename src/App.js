a4ad9da06f630ad4799d14b130fd9db16c47bae2
Baibhav KC <baibhavkc11@gmail.com>
2023-07-02 19:33:05 +0545

    added few components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import { Navbar } from './components/Navbar';
+import Home from './screens/Home';
+import { Login } from './screens/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
