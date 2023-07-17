6766cc839172c49fce804a30410726096d010c4c
Baibhav KC <baibhavkc11@gmail.com>
2023-07-17 18:04:14 +0545

    created routes and added post user route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoDB = require('./db');
 require('colors');
+const routes = require('./routes/CreateUser');
 
 const app = express();
 const port = 5000;
@@ -10,6 +11,9 @@ mongoDB();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.use(express.json());
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`App listening to port ${port}`.yellow);
