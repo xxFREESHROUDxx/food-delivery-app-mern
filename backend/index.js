4c6191142cb029a2dcc1d578e5b27f9e3db8367d
Baibhav KC <baibhavkc11@gmail.com>
2023-08-06 13:40:20 +0545

    changes for backend for signup api

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,12 @@ const port = 5000;
 
 mongoDB();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
