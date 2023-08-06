0a4b4d7d8d04a37ba2c0adb905402331c4ab5bb3
Baibhav KC <baibhavkc11@gmail.com>
2023-08-06 13:39:23 +0545

    small change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,13 +23,18 @@ export const Navbar = () => {
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
               <li className='nav-item'>
-                <Link className='nav-link active' aria-current='page' to='/'>
+                <Link className='nav-link' aria-current='page' to='/'>
                   Home
                 </Link>
               </li>
               <li className='nav-item'>
                 <Link className='nav-link' to='/login'>
                   Login
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/signup'>
+                  SignUp
                 </Link>
               </li>
             </ul>
