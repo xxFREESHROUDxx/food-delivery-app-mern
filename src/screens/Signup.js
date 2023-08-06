7176ce8f52aa08a4f52bac9cca563c78d09b4a12
Baibhav KC <baibhavkc11@gmail.com>
2023-08-06 13:40:01 +0545

    added signup page

---
+++ src/screens/Signup.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+  const [credentials, setCredentials] = useState({
+    name: '',
+    email: '',
+    password: '',
+    geolocation: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:5000/api/createuser', {
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: credentials.name,
+        email: credentials.email,
+        password: credentials.password,
+        location: credentials.geolocation,
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+
+    if (!json.success) {
+      alert('Enter a Valid Credentials!');
+    }
+  };
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: [e.target.value] });
+  };
+
+  return (
+    <div className='container mt-5 '>
+      <form onSubmit={handleSubmit}>
+        <div className='mb-3'>
+          <label htmlFor='name' className='form-label'>
+            Name
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            name='name'
+            onChange={handleChange}
+            value={credentials.name}
+            placeholder='Enter your name'
+          />
+        </div>
+
+        <div className='mb-3'>
+          <label htmlFor='email' className='form-label'>
+            Email address
+          </label>
+          <input
+            type='email'
+            className='form-control'
+            name='email'
+            value={credentials.email}
+            onChange={handleChange}
+            placeholder='Enter your email'
+          />
+        </div>
+
+        <div className='mb-3'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            className='form-control'
+            name='password'
+            onChange={handleChange}
+            value={credentials.password}
+            placeholder='Enter Password'
+          />
+        </div>
+
+        <div className='mb-3'>
+          <label htmlFor='geolocation' className='form-label'>
+            Address
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            name='geolocation'
+            onChange={handleChange}
+            value={credentials.geolocation}
+            placeholder='Enter your Address'
+          />
+        </div>
+
+        <button type='submit' className='m-3 btn btn-primary'>
+          Submit
+        </button>
+        <Link to='/login' className='m-3 btn btn-danger'>
+          Already a User?
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
